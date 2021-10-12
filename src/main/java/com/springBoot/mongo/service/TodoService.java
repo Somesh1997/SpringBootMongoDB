b77260d7584c3ccb9bfd6623f0326d0371f9e457
Somesh1997 <somesh6g@gmail.com>
2021-10-13 00:02:08 +0530

    Final Project is completed

--- src/main/java/com/springBoot/mongo/service/TodoService.java
+++ src/main/java/com/springBoot/mongo/service/TodoService.java
@@ -1,9 +1,7 @@
 package com.springBoot.mongo.service;
 
 import java.util.List;
-
 import javax.validation.ConstraintViolationException;
-
 import com.springBoot.mongo.exception.TodoCoollectionException;
 import com.springBoot.mongo.model.User;
 
@@ -15,5 +13,7 @@ public interface TodoService {
 	public User getSingleTodo(String id) throws TodoCoollectionException;
 
 	public void updateTodo(String id, User user) throws TodoCoollectionException;
+
 	public void deleteTodoById(String id) throws TodoCoollectionException;
+
 }
